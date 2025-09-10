ef84278bc5febf93be7f12d3dfa6a4bb8e5ba5bf
ANKIT THAKUR <ankitthakur960ak@gmail.com>
2025-09-10 21:13:50 +0530

    Add files via upload

---
+++ Front-End Development - Copy/Java Script/23deep_shalloe_copy.js
@@ -0,0 +1,12 @@
+// const user1 = {
+//     firstName: 'piyush',
+//     lastName: 'thakur',
+// }
+// let user2 = user1;
+
+
+const name1 = 'Ankit';
+
+let name2 = name1;
+
+name2 = name2 + ' Thakur';
