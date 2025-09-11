7e04a4d03207865ac730295592693774841fb18f
ANKIT THAKUR <ankitthakur960ak@gmail.com>
2025-09-11 10:08:21 +0530

    Update 25whileLoop.js

--- Front-End Development - Copy/Java Script/25whileLoop.js
+++ Front-End Development - Copy/Java Script/25whileLoop.js
@@ -1,17 +1,17 @@
 // debugger
-// console.log("Program Started!!!");
-// let count = 1;
-// while (count <= 5) {
-//     console.log("count : " + count);
-//     count++; // increment!
-// }    // Output: Count: 1, Count: 2, Count: 3, Count: 4, Count: 5
+console.log("Program Started!!!");
+let count = 1;
+while (count <= 5) {
+    console.log("count : " + count);
+    count++; // increment!
+}    // Output: Count: 1, Count: 2, Count: 3, Count: 4, Count: 5
 
 
-// let x = 1;
-// while (x <= 5) {
-//     console.log(x);
-//     x++;
-// }
+let x = 1;
+while (x <= 5) {
+    console.log(x);
+    x++;
+}
 
 const arr = ['Apple', 'Mango', 'Banana', 'Grapes', 'Coconut'];
 let i = 0; // initilization
@@ -21,4 +21,5 @@ while (i < arr.length) { // condition
     i++; //increment
 
 }
+
 console.log("Program End!!!");
